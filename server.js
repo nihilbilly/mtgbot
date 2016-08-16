1dc147235a9d918524f75c6590a9e036a1e95d09
Andrew Glaser <andrew.glaser@jhu.edu>
2016-08-16 14:24:38 -0400

    mtgapi went down so now we use mtgjson.com Allsets.json for our card data

--- server.js
+++ server.js
@@ -2,126 +2,112 @@ var fs = require('fs'),
 http = require('http'),
 Slack = require('slack-node'),
 Client = require('websocket').client,
-Index = require('node-index');
+Index = require('node-index'),
+path = require('path');
 
-var slack = new Slack("xoxb-34044266993-5Nnr2MuFUBS5JwGaPxLwK67i"),
+process.on('uncaughtException', function (err) {
+	console.log('Uncaught exception: ', err);
+});
+
+var slack = new Slack("REDACTED"),
 client = new Client(),
 index = new Index(),
 userId = null;
 
 console.log('Fetching list of card names...');
+var sets = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'AllSets.json'), 'utf8'));
 
-http.get('http://api.mtgapi.com/v2/names', function(res) {
-	var output = '';
-	
-	res.on('data', function (chunk) {
-		output += chunk;
-	});
-	
-	res.on('end', function() {
-		var names = JSON.parse(output).names;
+console.log('Indexing card names...');
+
+for (var s in sets) {
+	var set = sets[s];
+	for (var c in set.cards) {
+
+		var card = set.cards[c];
+		if (card.multiverseid !== undefined){
+			index.addDocument(card.id, { name: card.name, multiverseid: card.multiverseid.toString() });
+		}
+	}
+}
+
+console.log('Ready for requests...');
+
+client.on('connectFailed', function(error) {
+	console.log('error connecting', error);
+});
+
+client.on('connect', function(connection) {
+	connection.on('message', function(message) {
 		
-		console.log('Indexing card names...');
-		
-		for (id in names) {
-			index.addDocument(id, {name: names[id]});
+		data = JSON.parse(message.utf8Data);	
+		if (data.user != userId && data.type == 'message') {
+			if (!data.text){
+				return;
+			}
+			var text = data.text.replace(/(\[){2,}/, "[");
+			text = text.replace(/(\]){2,}/, "]");
+			var attachments = [];
+			var bestMatches = [];
+			var cardMatches = [];
+			var totalRequests = 0;
+			
+			for (i = text.indexOf("["); i >= 0; i = text.indexOf("[", i + 1)) {
+				cardMatches.push(text.substring(i + 1, text.indexOf("]", i)));
+			}
+			
+			if (!cardMatches.length && data.user == 'U0HP4K8D7' && text.indexOf("soup") >= 0) {
+				slack.api("chat.postMessage", {channel: data.channel, as_user: true, text: ':stew:'}, function() {});
+				return;
+			}
+			
+			for (i in cardMatches){
+				var indexMatches = index.query(cardMatches[i]),
+				target = cardMatches[i].toLowerCase().replace(/[^ \w]/g, ' ').replace(/ +/g, ' ');
+				var match = null;
+				
+				for (j in indexMatches) {
+					
+					var test = indexMatches[j].doc.name.toLowerCase().replace(/[^ \w]/g, ' ').replace(/ +/g, ' ');
+					
+					if (target.indexOf(test) >= 0 && (!match || indexMatches[j].doc.name.length > match.doc.name.length)) {
+						match = indexMatches[j];
+					}
+				}
+				if (match) {
+					bestMatches.push(match);
+				}
+			}
+			
+			totalRequests = bestMatches.length;
+			for (var i in bestMatches) {
+				(function(index, channel) {
+					var output = '';
+					
+					var card = bestMatches[index].doc;
+					attachments.push({
+						title: card.name,
+						title_link: 'http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid='+card.multiverseid,
+						image_url: 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid='+card.multiverseid+'&type=card'
+					});
+
+					totalRequests--;
+					if (totalRequests <= 0){
+						var JSONattachments = JSON.stringify(attachments);
+						slack.api("chat.postMessage", {channel: channel, as_user: true, text: ' ', attachments: JSONattachments}, function() {});
+					}
+				})(i, data.channel);
+			}
+			
 		}
 		
-		console.log('Ready for requests...');
 		
-		client.on('connectFailed', function(error) {
-			console.log('error connecting', error);
-		});
-		
-		client.on('connect', function(connection) {
-			connection.on('message', function(message) {
-				
-				data = JSON.parse(message.utf8Data);
-				//console.log(data);
-				if (data.user != userId && data.type == 'message') {
-					if (!data.text){
-						return;
-					}
-					var attachments = [];
-					var bestMatches = [];
-					var cardMatches = [];
-					var totalRequests = 0;
-					for (i = data.text.indexOf("["); i >= 0; i = data.text.indexOf("[", i + 1)) {
-						cardMatches.push(data.text.substring(i + 1, data.text.indexOf("]", i)));
-					}
-					if (!cardMatches.length && data.user === 'bmp' && data.text.indexOf("soup") >= 0) {
-						slack.api("chat.postMessage", {channel: channel, as_user: true, text: ':stew:'}, function() {});
-						return;
-					}
-					//console.log("cardMatches: ", cardMatches);
-					for (i in cardMatches){
-						var indexMatches = index.query(cardMatches[i]),
-						target = cardMatches[i].toLowerCase().replace(/[^ \w]/g, ' ').replace(/ +/g, ' ');
-						var match = null;
-						
-						//console.log("indexMatches: ", indexMatches)
-						for (j in indexMatches) {
-							
-							var test = indexMatches[j].doc.name.toLowerCase().replace(/[^ \w]/g, ' ').replace(/ +/g, ' ');
-							//console.log(indexMatches[j]);
-							
-							if (target.indexOf(test) >= 0 && (!match || indexMatches[j].doc.name.length > match.doc.name.length)) {
-								match = indexMatches[j];
-							}
-						}
-						if (match) {
-							bestMatches.push(match);
-						}
-					}
-					
-					totalRequests = bestMatches.length;
-					//console.log("bestMatches: ", bestMatches);
-					for (var i in bestMatches) {
-						(function(index, channel) {
-							http.get('http://api.mtgapi.com/v2/cards?name='+bestMatches[index].doc.name, function(res) {
-								var output = '';
-								//console.log("bestMatch in get: ", bestMatches[index], index);
-								
-								res.on('data', function (chunk) {
-									output += chunk;
-								});
-								
-								res.on('end', function() {
-									var card = JSON.parse(output).cards[0];
-									//sometimes the id is 0 so we get a bad link. weird.
-									if (card.multiverseid === 0){
-										card = JSON.parse(output).cards[1];
-									}
-									//console.log(card);
-									attachments.push({
-										title: bestMatches[index].doc.name,
-										title_link: 'http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid='+card.multiverseid,
-										image_url: card.images.gatherer
-									});
-									//console.log("attachments: ", attachments);
-									//console.log(index, totalRequests);
-									totalRequests--;
-									if (totalRequests <= 0){
-										var JSONattachments = JSON.stringify(attachments);
-										slack.api("chat.postMessage", {channel: channel, as_user: true, text: ' ', attachments: JSONattachments}, function() {});
-									}
-								});
-								
-								
-							});
-						})(i, data.channel);
-					}
-					
-				}
-				
-				
-			});
-		});
-		
-		slack.api("rtm.start", function(err, response) {
-			userId = response.self.id;
-			client.connect(response.url);
-		});
 	});
 });
 
+slack.api("rtm.start", function(err, response) {
+	if (err){
+		console.log("Error starting slack bot: ", err);
+	}
+	userId = response.self.id;
+	client.connect(response.url);
+});
